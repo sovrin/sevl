e6f4e244a85ea47a9e695d4e17e3fc860e225f80
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2023-11-24 22:05:50 +0100

    feat(index): load multiple env

--- src/index.ts
+++ src/index.ts
@@ -2,89 +2,118 @@ import {resolve} from 'path';
 import stream from './stream';
 import {parser, reader} from './transformer';
 
-type Variables<T> = Record<keyof T, string> | Record<string, never>;
 type Options = {
     bufferSize: number,
     cwd: string,
 };
+type Variables<T> = NodeJS.ProcessEnv & T;
+type Entry = { type: typeof Type[keyof typeof Type], variables: object };
 
-/**
- *
- * @param options
- */
-const factory = <T extends Variables<T>>(options: Partial<Options> = {}): Promise<Variables<T>> => {
+const Type = {
+    ENVIRONMENT: 'environment',
+    DEFAULT: 'default',
+    LOCAL: 'local',
+} as const;
+const BASE_ENV = '.env';
+const ORDER = ['environment', 'default', 'local'];
+
+const factory = <T>(options: Partial<Options> = {}): Promise<Variables<T>> => {
     const {
         bufferSize = 1024 * 32,
         cwd = process.cwd(),
     } = options;
 
-    /**
-     *
-     */
-    const getEnv = () => {
-        const {NODE_ENV} = process.env;
-        let env = '.env';
-
-        if (NODE_ENV === 'production') {
-            env = '.env.production';
-        } else if (NODE_ENV === 'development') {
-            env = '.env.development';
-        } else if (NODE_ENV === 'test') {
-            env = '.env.test';
-        }
-
-        return resolve(cwd, env);
+    const sort = (result: Array<Entry>) => {
+        return result.sort((a, b) => {
+            return ORDER.indexOf(a.type) - ORDER.indexOf(b.type);
+        })
     }
 
-    const env = getEnv();
-    const variables = <Variables<T>>{};
+    const pluck = (result: Array<Entry>) => {
+        return result.map(({variables}) => variables);
+    }
 
-    return new Promise((resolve): void => {
-        /**
-         *
-         * @param variables
-         */
-        const merge = (variables) => {
-            return {
-                ...variables,
-                ...process.env,
-            }
-        }
+    const map = (entries: Array<NodeJS.Dict<string>>): Promise<Variables<T>> => {
+        const variables = {} as Promise<Variables<T>>;
 
-        /**
-         *
-         * @param key
-         * @param value
-         */
-        const onData = ([key, value]: [string, string]): void => {
-            if (!process.env[key]) {
+        for (const entry of entries) {
+            for (const [key, value] of Object.entries(entry)) {
+                if (process.env[key]) {
+                    continue;
+                }
+
                 process.env[key] = value;
                 variables[key] = value;
             }
-        };
+        }
 
-        /**
-         *
-         */
-        const onEnd = (): void => {
-            resolve(merge(variables));
+        return {
+            ...variables,
+            ...process.env,
         };
+    }
 
-        /**
-         *
-         */
-        const onError = (): void => {
-            resolve(merge(variables));
-        };
+    const read = (path: string, type: typeof Type[keyof typeof Type]): Promise<Entry> => {
+        return new Promise((resolve): void => {
+            const variables = {};
 
-        stream(env, bufferSize)
-            .on('error', onError)
-            .pipe(reader())
-            .pipe(parser())
-            .on('data', onData)
-            .on('end', onEnd)
-        ;
-    });
+            const onData = ([key, value]: [string, string]): void => {
+                variables[key] = value;
+            };
+
+            const onEnd = (): void => {
+                resolve({
+                    type,
+                    variables,
+                });
+            };
+
+            const onError = (): void => {
+                resolve({
+                    type,
+                    variables,
+                });
+            };
+
+            stream(path, bufferSize)
+                .on('error', onError)
+                .pipe(reader())
+                .pipe(parser())
+                .on('data', onData)
+                .on('end', onEnd)
+            ;
+        })
+    }
+
+    const instructions: Array<[string, typeof Type[keyof typeof Type]]> = [
+        (() => {
+            let path = resolve(cwd, BASE_ENV);
+
+            const {NODE_ENV} = process.env;
+            if (NODE_ENV !== undefined && NODE_ENV !== '') {
+                path += `.${NODE_ENV}`;
+            }
+
+            return [path, Type.ENVIRONMENT];
+        })(),
+        (() => {
+            const path = resolve(cwd, BASE_ENV);
+
+            return [path, Type.DEFAULT];
+        })(),
+        (() => {
+            const path = resolve(cwd, BASE_ENV + '.local');
+
+            return [path, Type.LOCAL];
+        })()
+    ];
+
+    const promises = instructions.map((args) => read.apply(null, args));
+
+    return Promise.all(promises)
+        .then(sort)
+        .then(pluck)
+        .then(map);
 };
 
 /**
